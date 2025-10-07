6e6086f4a454a8f0e0db95cbd57ae339b687a35d
Helyio <helyio.dev@gmail.com>
2025-10-07 07:50:16 +0200

    update

--- optimization/Temperature/Python.py
+++ optimization/Temperature/Python.py
@@ -1,9 +1,2 @@
-n=input()
-s=input().split(' ')
-l=5526
-try:
- for m in s:
-  m=float(m)
-  if abs(m)<abs(l)or abs(m)==abs(l)and m>l:l=int(m)
- print(l)
-except:print(0)
+input()
+print(min(input().split(),key=lambda x:abs(int(x)-.1),default=0))
