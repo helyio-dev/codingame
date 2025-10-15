2adab3774273ba39f88b4085a240c7cd6de1326a
Helyio <helyio.dev@gmail.com>
2025-10-16 01:10:55 +0200

    update

---
+++ puzzle/easy/Organic Compounds 🔬/Java.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        in.nextLine();
+
+        String[] compounds = new String[N];
+        for (int i = 0; i < N; i++) {
+            compounds[i] = in.nextLine();
+        }
+
+        boolean found = true;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < compounds[i].length() - 2; j++) {
+                if (compounds[i].substring(j, j + 2).equals("CH")) {
+                    int bonds = Integer.parseInt("" + compounds[i].charAt(j + 2));
+                    try {
+                        bonds += Integer.parseInt("" + compounds[i - 1].charAt(j + 1));
+                    } catch (Exception ignored) {
+                    }//Don't like it but it's easier
+                    try {
+                        bonds += Integer.parseInt("" + compounds[i + 1].charAt(j + 1));
+                    } catch (Exception ignored) {
+                    }
+                    try {
+                        bonds += Integer.parseInt("" + compounds[i].charAt(j - 2));
+                    } catch (Exception ignored) {
+                    }
+                    try {
+                        bonds += Integer.parseInt("" + compounds[i].charAt(j + 4));
+                    } catch (Exception ignored) {
+                    }
+                    if (bonds != 4) {
+                        found = false;
+                    }
+                }
+                j += 5;
+            }
+        }
+
+        System.out.println(found ? "VALID" : "INVALID");
+    }
+}
---
+++ puzzle/easy/orDer oF succeSsion/Java.java
@@ -0,0 +1,100 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
+class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        Node root = null;
+        for (int i = 0; i < n; i++) {
+            Node node = new Node(in);
+            if (root == null) {
+                root = node;
+            } else {
+                root.addNode(node);
+            }
+        }
+
+        downFirst(root);
+    }
+
+    private static void downFirst(Node node) {
+        if (node.isVisible()) {
+            System.out.println(node.name);
+        }
+
+        if (node.hasChildren()) {
+            for (int i = 0; i < node.children.size(); i++) {
+                downFirst(node.children.get(i));
+            }
+        }
+    }
+}
+
+class Node {
+    String name;
+    String parent;
+    Integer birth;
+    boolean alive;
+    String religion;
+    String gender;
+
+    List<Node> children;
+
+    Node(Scanner in) {
+        this.name = in.next();
+        this.parent = in.next();
+        this.birth = in.nextInt();
+        this.alive = in.next().equals("-");
+        this.religion = in.next();
+        this.gender = in.next();
+        children = new ArrayList<>();
+    }
+
+    boolean addNode(Node node) {
+        if (node.parent.equals("-")) {
+            return false;
+        } else if (node.parent.equals(this.name)) {
+            this.children.add(node);
+            this.children.sort(Comparator.comparing(Node::getGender).reversed().thenComparing(Node::getBirth));
+            return true;
+        } else {
+            for (Node child : children) {
+                boolean b = child.addNode(node);
+                if (b) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    boolean hasChildren() {
+        return !children.isEmpty();
+    }
+
+    private String getGender() {
+        return gender;
+    }
+
+    private Integer getBirth() {
+        return birth;
+    }
+
+    private boolean isAlive() {
+        return this.alive;
+    }
+
+    private boolean isAnglican() {
+        return religion.equals("Anglican");
+    }
+
+    boolean isVisible() {
+        return isAlive() && isAnglican();
+    }
+}
