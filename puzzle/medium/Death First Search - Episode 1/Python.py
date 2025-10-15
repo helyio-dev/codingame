09b34185af8c5e747f8f04c804a339f8175ea29a
Helyio <helyio.dev@gmail.com>
2025-10-15 22:20:40 +0200

    update

--- puzzle/medium/Death First Search - Episode 1/Python.py
+++ puzzle/medium/Death First Search - Episode 1/Python.py
@@ -1,34 +1,35 @@
-import sys
-import math
-
 n, l, e = [int(i) for i in input().split()]
+links = [set() for node in range(n)]
+exits = set()
 
-links = {i: set() for i in range(n)}
-for _ in range(l):
+for i in range(l):
     n1, n2 = [int(j) for j in input().split()]
     links[n1].add(n2)
     links[n2].add(n1)
 
-gateways = set()
-for _ in range(e):
-    ei = int(input())
-    gateways.add(ei)
+for i in range(e):
+    exits.add(int(input()))
+
+def pathToNearestExit(a):
+    visited = [False for node in range(n)]
+    q = [a]
+    path = {a: []}
+
+    while len(q):
+        node = q.pop(0)
+        if node in exits:
+            return path[node]
+
+        for neighbor in links[node]:
+            if neighbor not in path:
+                path[neighbor] = path[node] + [neighbor]
+                q.append(neighbor)
 
 while True:
-    si = int(input())  
-
-    found = False
-    for neighbor in links[si]:
-        if neighbor in gateways:
-            print(f"{si} {neighbor}")
-            found = True
-            break
-
-    if not found:
-        for gw in gateways:
-            for neighbor in links[gw]:
-                print(f"{neighbor} {gw}")
-                found = True
-                break
-            if found:
-                break
+    si = int(input())
+    path = [si]+pathToNearestExit(si)
+    i = 1 if len(path) > 2 else 0
+    a,b = path[i],path[i+1]
+    print(a, b)
+    links[a].remove(b)
+    links[b].remove(a)
