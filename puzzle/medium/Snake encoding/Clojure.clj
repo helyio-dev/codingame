386b82e011a08702cb33c7640ed2717c64c18b98
Helyio <helyio.dev@gmail.com>
2025-10-17 09:55:24 +0200

    update

---
+++ puzzle/medium/Snake encoding/Clojure.clj
@@ -0,0 +1,21 @@
+(ns Solution
+  (:require [clojure.string :as str])
+  (:gen-class))
+
+
+(defn -main [& args]
+  (let [size (read)
+        i (read)
+        _ (read-line)
+        n (* size size)]
+    (->> (repeatedly size read-line)
+         (apply map vector)
+         (mapcat #(%1 %2) (cycle [reverse identity]))
+         cycle
+         (drop (- n i))
+         (take n)
+         (partition size)
+         (map #(%1 %2) (cycle [reverse identity]))
+         (apply map str)
+         (str/join \newline)
+         println)))
---
+++ puzzle/medium/The fastest/Clojure.clj
@@ -0,0 +1,13 @@
+
+(ns Solution
+  (:gen-class))
+
+(defn seconds [time]
+  (->> (clojure.string/split time #":")
+       (map #(Integer/valueOf %))
+       (map * [3600 60 1])
+       (reduce +)))
+
+(defn -main [& _]
+  (let [n (read) _ (read-line)]
+    (println (apply min-key seconds (repeatedly n read-line)))))
